c9b3f8df72c8d179d99fa6a6e4759fb8e83cfb1d
jnoortheen <jnoortheen@gmail.com>
2017-08-24 20:44:36 +0530

    change 'test_specified_fields' to check for reverse_accessor fields

--- django_admin_smoke_tests/tests.py
+++ django_admin_smoke_tests/tests.py
@@ -181,6 +181,7 @@ class AdminSiteSmokeTestMixin(object):
             has_form_field = attr in form_field_names
             has_model_class_attr = hasattr(model_instance.__class__, attr)
             has_admin_attr = hasattr(model_admin, attr)
+            has_reverse_accessor = hasattr(model_instance, attr + '_set')
 
             try:
                 has_model_attr = hasattr(model_instance, attr)
@@ -188,7 +189,8 @@ class AdminSiteSmokeTestMixin(object):
                 has_model_attr = attr in model_instance.__dict__
 
             has_field_or_attr = has_model_field or has_form_field or\
-                has_model_attr or has_admin_attr or has_model_class_attr
+                has_model_attr or has_admin_attr or has_model_class_attr\
+                or has_reverse_accessor
 
             self.assertTrue(has_field_or_attr, '%s not found on %s (%s)' %
                 (attr, model, model_admin,))
