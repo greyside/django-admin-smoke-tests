166fffdb5893ed1307273152fb35b53732c6f86e
jnoortheen <jnoortheen@gmail.com>
2017-12-15 20:18:10 +0530

    chore: updates

--- fabfile.py
+++ fabfile.py
@@ -1,23 +1,13 @@
 from fabric.api import task
 from fabric.operations import local
 
-import utils_plus
+import django_admin_smoke_tests
 
 
 @task
 def release():
     local('git push')
-    local('git tag {}'.format(utils_plus.__version__))
+    local('git tag {}'.format(django_admin_smoke_tests.__version__))
     local('git push --tags')
 
-    # dont forget to have this file
-    # ~/.pypirc
-    # [distutils]
-    # index-servers =
-    #  pypi
-
-    # [pypi]
-    # repository: https://upload.pypi.org/legacy/
-    # username: jnoortheen
-    # password: pwd
     local('python setup.py sdist upload')
